60e55e65aa701451c7e0fb1e0fa25ec843817dfa
mayorov <may.viktor@gmail.com>
2018-03-10 01:34:13 +0300

    + express server

--- server/server.js
+++ server/server.js
@@ -2,10 +2,14 @@ import express from 'express';
 import path from 'path';
 
 let app = express();
-app.get('/*', (req, res) => {
-    // res.send('he');
-    console.log(path.join(__dirname, '../src/index.html'));
-    res.sendFile(path.join(__dirname, '../src/index.html'));
+
+app.use(express.static(path.join(__dirname, '../src'), {
+    maxAge: 30 * 60 * 60 * 24 * 1000
+}));
+
+app.get('*', function (req, res) {
+    res.sendFile(__dirname + 'index.html');
 });
+
 
 app.listen(3000, () => console.log("express server is running on 3000"));
